6943b9381a097e28e38e0d0a93c31ebd0a91d279
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2024-05-10 16:51:53 +0200

    test

--- app/src/main/java/com/example/appleadtech/MainActivity.kt
+++ app/src/main/java/com/example/appleadtech/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.appleadtech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.example.appleadtech.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var editText = binding.editTextName
+        var button = binding.button
+        var textView = binding.textName
+
+
+        binding.button.setOnClickListener {
+            var str = editText.text.toString()
+            textView.text = str
+        }
 
     }
+
+
+
+
+
 }
